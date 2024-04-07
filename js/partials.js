97333b1462f056c18ec24bbd1e4aca61dc99c9a3
x-labz <info@x-labz>
2024-04-07 20:58:43 +0200

    2024.04.07

--- js/partials.js
+++ js/partials.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-Handlebars.partials['czs_kz.hbs'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"row about-wrapper\">\r\n  <img\r\n    class=\"col s5 m2 responsive-img img-border\"\r\n    style=\"margin-top: 5px;\"\r\n    src=\"img/CZsuzsi-300px.jpg\"\r\n  />\r\n\r\n  <p class=\"col s6 m4 light justify-text\">\r\n    <a href=\"https://www.facebook.com/czvikliizs\"><b>Czvikli Zsuzsanna</b></a>\r\n    (Gerincjóga, Hatha jóga és Jóga flow oktató, Fasciális jógaterapeuta, Jóga\r\n    mozgásterapeuta, Astanga jógaoktató)\r\n  </p>\r\n\r\n  <p class=\"col s6 m4 light justify-text\">\r\n    <a href=\"https://www.facebook.com/kosazoe\"><b>Kósa Zoé</b></a>\r\n    (Hatha jóga, Vinyasa flow és Yin jógaoktató, kortárs és modern táncművész)\r\n  </p>\r\n  <img\r\n    class=\"col s5 m2 responsive-img img-border\"\r\n    style=\"margin-top: 5px;\"\r\n    src=\"img/zoé_300px.jpg\"\r\n  />\r\n\r\n</div>";
+templates['czs.hbs'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"row about-wrapper\">\r\n  <img\r\n    class=\"col s5 m2 responsive-img img-border\"\r\n    style=\"margin-top: 5px;\"\r\n    src=\"img/CZsuzsi-300px.jpg\"\r\n  />\r\n\r\n  <p class=\"col s6 m4 light justify-text\">\r\n    <a href=\"https://www.facebook.com/czvikliizs\"><b>Czvikli Zsuzsanna</b></a>\r\n    (Gerincjóga, Hatha jóga és Jóga flow oktató, Fasciális jógaterapeuta, Jóga\r\n    mozgásterapeuta, Astanga jógaoktató)\r\n  </p>\r\n\r\n\r\n\r\n</div>";
 },"useData":true});
 })();
